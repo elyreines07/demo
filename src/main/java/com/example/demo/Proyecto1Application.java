0b7f446984fbcf1e4b89e7353e3dc6b429aa246d
Nancy Villena <nancyvillena07@gmail.com>
2023-03-06 18:21:20 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/Proyecto1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1Application {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1Application.class, args);
+		System.out.println("HOLA MUNDO!");
+	}
+
+}
